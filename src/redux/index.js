a505589a8542a5b33fc804c34ee8103b8be3bcdb
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-07-12 12:27:56 -0300

    feat: create counter slice and pass to store

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './modules/counter';
 
 const reducers = {
-
+  counter: counterSlice.reducer,
 };
 
 const store = configureStore({

bfb490417ace086db2240c8ee4004079f20cbb28
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-07-12 12:42:15 -0300

    implement actions into app.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import counterSlice from './redux/modules/counter';
 
 function App() {
   const { counter } = useSelector((state) => state.counter);
+  const { decrementCounter, incrementCounter } =  counterSlice.actions;
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <p>{counter}</p>
+      <button onClick={() => dispatch(incrementCounter())}>Increment</button>
+      <button onClick={() => dispatch(decrementCounter())}>Decrement</button>
     </div>
   );
 }
